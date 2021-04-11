051d97d9a0b33e3c5019cb840ee25d5479ff63a7
Agafonov Andrew <pentri4@gmail.com>
2021-04-11 23:10:57 +0700

    Add suggestion box cmp.

---
+++ src/components/suggetionBox/SuggetionBox.js
@@ -0,0 +1,16 @@
+/**
+* Module exports view of suggestion phrases list.
+* */
+
+import React from 'react';
+
+/**
+* Component renders suggestion phrases list.
+* @param {Object} props - component's props.
+* @param {String} props.word - begin of phrase.
+* */
+const SuggestionBox = props => {
+
+};
+
+export default SuggestionBox;
